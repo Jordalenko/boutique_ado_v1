5ea59b2ca9ad6a60c20f9a9e0eb7f183a0948288
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-07-02 21:07:18 +0100

    remove httpresponse message from urls.py. Add static files to media folder. Add css to style index.html. Add homepage image.

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', lambda request: HttpResponse("Homepage is working!")),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
