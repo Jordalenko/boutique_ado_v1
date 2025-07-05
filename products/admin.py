e58f0db75132364eedf071c4555bde4b4c59a817
JordanAcomba <jordan.acomba@students.codeinstitute.net>
2025-07-05 21:35:26 +0100

    Add product and category list display to admin.py. Recreate loaddata and superuser

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Product, Category
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'category',
+        'price',
+        'rating',
+    )
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    def get_friendly_name(self, obj):
+        return obj.get_friendly_name()
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
